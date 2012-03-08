6e386332c4942e1bed05ed7346b1932f966118c8
Nicolas VERINAUD <n.verinaud@gmail.com>
2012-03-08 16:23:30 +0100

    Initial Commit.

---
+++ Project/SDLKit/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SDLKit
+//
+//  Created by Nicolas Verinaud on 08/13/2012.
+//
+
+#include <stdlib.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_opengl.h>
+
+#include "main.h"
+
+int SDL_main (int argc, char **argv) 
+{
+	// your code here...
+    return 0;
+}
